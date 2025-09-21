d74b5cd78003a15fc4c49bbaca4b618ac5a2be09
pnzrfst <jxssxp@gmail.com>
2025-09-21 08:57:44 -0300

    feat: create ClientsForm component

--- src/components/ClientForm/index.tsx
+++ src/components/ClientForm/index.tsx
@@ -1,11 +1,7 @@
-import {
-  InputLabel,
-  MenuItem,
-  Select,
-  TextField,
-} from "@mui/material";
+import { Modal, TextField } from "@mui/material";
 import "./index.css";
 import { useState } from "react";
+import { API } from "@/api";
 
 interface ClientsComponentProps {
   onSubmit: () => void;
@@ -13,6 +9,12 @@ interface ClientsComponentProps {
   isOpen: boolean;
   title: string;
   subtitle: string;
+}
+
+interface Client {
+  name: string;
+  phone: string;
+  address: string
 }
 
 export default function ClientsForm({
@@ -23,60 +25,86 @@ export default function ClientsForm({
   subtitle,
 }: ClientsComponentProps) {
 
+  function clearInputs() {
+    setName("");
+    setAddress("");
+    setPhone("");
+  }
+
+
+  async function handleCreateClient(event: React.FormEvent) {
+    event.preventDefault();
+
+    const newClient : Client = {
+      name,
+      phone,
+      address
+    }
+
+    try {
+      await API.post("/clients", newClient);
+      clearInputs();
+      onSubmit();
+    } catch (error: any) {
+      console.log("Erro ao criar cliente", error);
+    }
+  }
+
+
   const [address, setAddress] = useState<string>("");
   const [name, setName] = useState<string>("");
-  const [contact, setContact] = useState<string>("");
+  const [phone, setPhone] = useState<string>("");
 
   if (!isOpen) return null;
 
   return (
-    <div className="overlay">
-      <div className="formBody">
-        <span className="title">
-          <h1>{title}</h1>
-          <p>{subtitle}</p>
-        </span>
-        <form className="inputs" onSubmit={onSubmit}>
-          <TextField
-            type="text"
-            required
-            label="Nome do cliente"
-            id="name"
-            value={name}
-            variant="outlined"
-            onChange={(e) => setName(e.target.value)}
-            autoComplete="off"
-          />
-          <TextField
-            type="text"
-            required
-            value={address}
-            label="Endereço"
-            id="address"
-            onChange={(e) => setAddress(e.target.value)}
-            variant="outlined"
-            autoComplete="off"
-          />
-          <TextField
-            type="number"
-            required
-            value={contact}
-            label="Contato"
-            id="contact"
-            onChange={(e) => setContact(e.target.value)}
-            variant="outlined"
-            autoComplete="off"
-          />
-        </form>
-        <div className="btnsAction">
-          <button id="saveButton">
-            Salvar
-          </button>
-          <button id="cancelButton" onClick={onCancel}>
-            Cancelar
-          </button>
-        </div>
+    <Modal className="modalBody" open={isOpen} onClose={onCancel}>
+      <div>
+        <Modal className="modalBody" open={isOpen} onClose={onCancel}>
+          <form
+            className="formBody"
+            onSubmit={handleCreateClient}
+          >
+            <h1>{title}</h1>
+            <p>{subtitle}</p>
+            <div className="inputArea">
+              <TextField
+                type="text"
+                placeholder="Nome"
+                className="inputItem"
+                label="Nome do cliente"
+                value={name}
+                onChange={(e) => setName(String(e.target.value))}
+              ></TextField>
+              <TextField
+                type="text"
+                placeholder="Contato"
+                className="inputItem"
+                label="Contato do cliente (Número de telefone)"
+                value={phone}
+                onChange={(e) => setPhone(String(e.target.value))}
+              ></TextField>
+              <TextField
+                type="text"
+                placeholder="Endereço"
+                className="inputItem"
+                label="Endereço"
+                value={address}
+                onChange={(e) => setAddress(String(e.target.value))}
+              ></TextField>
+            </div>
+
+            <div className="btns-action">
+              <button type="submit" id="saveButton">
+                Salvar
+              </button>
+              <button type="button" id="cancelButton" onClick={onCancel}>
+                Cancelar
+              </button>
+            </div>
+          </form>
+        </Modal>
       </div>
-    </div>
+    </Modal>
   );
 }
