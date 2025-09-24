a9da789d5492909421bb8b9cc5565c451fd6e211
pnzrfst <jxssxp@gmail.com>
2025-09-24 18:34:19 -0300

    feat: ensure that table will handle pagination

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -1,29 +1,38 @@
-"use client"
-import { DataGrid, GridColDef, GridRowsProp} from '@mui/x-data-grid';
-
+"use client";
+import {
+  DataGrid,
+  GridColDef,
+  GridPaginationModel,
+  GridRowsProp,
+} from "@mui/x-data-grid";
 import "./index.css";
-
 
 interface TableComponentProps {
   columns: GridColDef[];
-  rows: GridRowsProp; 
+  rows: GridRowsProp;
+  paginationModel: GridPaginationModel;
+  onPaginationModelChange: (model: GridPaginationModel) => void;
+  rowCount: number;
 }
 
-export default function DataTable({ columns, rows }: TableComponentProps) {
+export default function DataTable({
+  rows,
+  columns,
+  paginationModel,
+  onPaginationModelChange,
+  rowCount,
+}: TableComponentProps) {
   return (
-    <div>
-      
-    <DataGrid rows = {rows} columns = {columns}
-     initialState = {{
-      pagination: {
-        paginationModel: {
-          page: 0,
-          pageSize: 5
-        }
-      }
-     }}
-     pageSizeOptions={[5, 10]} checkboxSelection
-     />
-     </div>
-  )
+    <div style={{ height: 400, width: "100%" }}>
+      <DataGrid
+        rowCount={rowCount}
+        rows={rows}
+        columns={columns}
+        paginationMode="server"
+        paginationModel={paginationModel}
+        onPaginationModelChange={onPaginationModelChange}
+        pageSizeOptions={[5, 10, 20]}
+      />
+    </div>
+  );
 }
